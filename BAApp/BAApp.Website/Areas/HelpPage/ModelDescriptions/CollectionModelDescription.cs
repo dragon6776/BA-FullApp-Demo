a3ac9184be40345276b7b00ac1f1c0a6f73d71cf
ADMIN <dragon6776>
2019-03-22 23:57:59 +0700

    first commit

---
+++ BAApp/BAApp.Website/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BAApp.Website.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BAApp/BAApp.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BAApp.Website.WebApiApplication" Language="C#" %>
